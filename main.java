6a67097565862aa15112de7c854283c42052a92e
Michael <170946@hkis.edu.hk>
2016-09-01 09:42:05 +0800

    added some comments

--- main.java
+++ main.java
@@ -3,12 +3,12 @@ public class main
 {
     public static void main(String args[])
     {
-        class1 r = new class1();
-        System.out.println(r.getData());
-        r.setData(1, 2, 3);
-        System.out.println(r.getData());
-        Scanner s = new Scanner(System.in);
-        r.setData(s.nextInt(),s.nextInt(),s.nextInt());
-        System.out.println(r.getData());
+        class1 r = new class1(); //creates object r of datatype class1
+        System.out.println(r.getData()); //prints r's properties
+        r.setData(1, 2, 3); //changes r's properties
+        System.out.println(r.getData()); //prints the changed version of r's properties
+        Scanner s = new Scanner(System.in); //creates a scanner to take in user input
+        r.setData(s.nextInt(),s.nextInt(),s.nextInt()); //changes r's properties to the user input
+        System.out.println(r.getData()); //prints r's properties to be the user input
     }
 }
