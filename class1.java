e1a835ee162e0125a53e30d3dcadf08088fe4655
Michael <170946@hkis.edu.hk>
2016-09-01 09:36:51 +0800

    init

---
+++ class1.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class class1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class class1
+{
+    // instance variables - replace the example below with your own
+    private int x, y, z;
+    
+    public class1()
+    {
+        // initialise instance variables
+        x = 0;
+        y = 0;
+        z = 0;
+    }
+
+    
+    public String getData()
+    {
+        // put your code here
+        return "" + x + "," + y + "," + z;
+    }
+    
+    public void setData(int a, int b, int c)
+    {
+        x = a;
+        y = b;
+        z = c;
+    }
+}
---
+++ main.java
@@ -0,0 +1,10 @@
+public class main
+{
+    public static void main(String args[])
+    {
+        class1 r = new class1();
+        System.out.println(r.getData());
+        r.setData(1, 2, 3);
+        System.out.println(r.getData());
+    }
+}
